3ca5f82e7421ea4f6da7b1316b8d3db13d2822eb
Xiaohua Yi <rogeryi@comcast.net>
2013-07-13 23:13:44 +0000

    small udate

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from Startup001!');
+  response.send('Hello World from Coursera/Startup Engineering!');
 });
 
 var port = process.env.PORT || 5000;
