99d6cd620f4184a093884825ec270bdeb30e356c
Xiaohua Yi <rogeryi@comcast.net>
2013-07-13 23:20:46 +0000

    yet another update

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from Coursera/Startup Engineering!');
+  response.send('Hi Andrew! Waht are you doing? --from Coursera/Startup Engineering!');
 });
 
 var port = process.env.PORT || 5000;
