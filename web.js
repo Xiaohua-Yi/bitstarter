b0bfa54ce6a16b4f944a99dab19140bbc6b4bab9
Xiaohua Yi <rogeryi@comcast.net>
2013-07-14 00:26:59 +0000

    message vis var

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 
-var app = express();
+var app = express.createServer(express.logger());
 
-var msg = 'Hi Andrew! What are you doing? --from Coursera/Startup Engineering!';
+var msg = 'message is assigned through a var';
 app.get('/', function(request, response) {
   response.send(msg);
 });
