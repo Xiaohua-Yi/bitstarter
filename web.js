866a1722333010c516b88e0b7836c6cb778b66be
Xiaohua Yi <rogeryi@comcast.net>
2013-07-14 01:22:49 +0000

    msg via fs.readFile

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
-var fs = require('fs');
+fs = require('fs')
 var express = require('express');
 var app = express.createServer(express.logger());
-var msg = fs.readFileSync('index.html');
+
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
+
 app.get('/', function(request, response) {
-  response.send(msg);
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
